bc7976f2c05ab31b0e3f2ebeb35b2176bfe5524a
Marek Kasztelnik <mkasztelnik@gmail.com>
2014-06-04 20:40:20 +0200

    first point

--- lib/tennis_game/gem.rb
+++ lib/tennis_game/gem.rb
@@ -1,5 +1,47 @@
 class Tennis::Gem
+  def initialize
+    @player_one = Player.new
+    @player_two = Player.new
+  end
+
   def score
-    'Love all'
+    if eq?
+      'Love all'
+    else
+      "#{@player_one.score}, #{@player_two.score}"
+    end
+  end
+
+  def player_one_scores
+    @player_one.score!
+  end
+
+  def player_two_scores
+    @player_two.score!
+  end
+
+  private
+
+  def eq?
+    @player_one.points == @player_two.points
+  end
+
+  class Player
+    attr_reader :points
+
+    def initialize
+      @points = 0
+    end
+
+    def score!
+      @points += 1
+    end
+
+    def score
+      case @points
+      when 0 then 'Love'
+      when 1 then 'Fifteen'
+      end
+    end
   end
 end
--- spec/tennis_game/gem_spec.rb
+++ spec/tennis_game/gem_spec.rb
@@ -5,4 +5,16 @@ describe Tennis::Gem do
   it 'starts from "Love all"' do
     expect(subject.score).to eq 'Love all'
   end
+
+  it 'scores "Fifteen, Love" when first user wins first ball' do
+    subject.player_one_scores
+
+    expect(subject.score).to eq 'Fifteen, Love'
+  end
+
+  it 'scores "Love, Fifteen" when second user wins first ball' do
+    subject.player_two_scores
+
+    expect(subject.score).to eq 'Love, Fifteen'
+  end
 end
