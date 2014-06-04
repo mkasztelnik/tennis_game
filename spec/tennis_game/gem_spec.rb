4953abd0a1994318898856d3367c4c3affc67c38
Marek Kasztelnik <mkasztelnik@gmail.com>
2014-06-04 20:57:02 +0200

    spec refactored

--- spec/tennis_game/gem_spec.rb
+++ spec/tennis_game/gem_spec.rb
@@ -1,39 +1,48 @@
 require 'tennis_game'
 
 describe Tennis::Gem do
-
   it 'starts from "Love all"' do
     expect(subject.score).to eq 'Love all'
   end
 
   it 'scores "Fifteen, Love" when first user wins first ball' do
-    subject.player_one_scores!
+    score(1, 0)
 
     expect(subject.score).to eq 'Fifteen, Love'
   end
 
   it 'scores "Love, Fifteen" when second user wins first ball' do
-    subject.player_two_scores!
+    score(0, 1)
 
     expect(subject.score).to eq 'Love, Fifteen'
   end
 
   it 'scores "Fifteen all" when 1:1' do
-    subject.player_one_scores!
-    subject.player_two_scores!
+    score(1, 1)
 
     expect(subject.score).to eq 'Fifteen all'
   end
 
   it 'scores "Thirty, Love" when 2:0' do
-    2.times { subject.player_one_scores! }
+    score(2, 0)
 
     expect(subject.score).to eq 'Thirty, Love'
   end
 
   it 'scores "Love, Forty" when 0:3' do
-    3.times { subject.player_two_scores! }
+    score(0, 3)
 
     expect(subject.score).to eq 'Love, Forty'
+  end
+
+  it 'scores "Deuce" when 3:3' do
+    score(3, 3)
+
+    expect(subject.score).to eq 'Deuce'
+  end
+
+  def score(player_one_points, player_two_points)
+    player_one_points.times { subject.player_one_scores! }
+    player_two_points.times { subject.player_two_scores! }
   end
 end
